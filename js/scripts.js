b47ffd86735205f95889a4cd9e996d023e70fefa
Gareth Grindeland <gt.grindeland@gmail.com>
2023-03-29 09:29:32 -0700

    Add js scripts file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 // User Interface Logic
 window.onload = function() {
-  let h1 = document.querySelector("h1");
-  h1.onmouseover = function() {
-    window.alert("I am a heading element.");
+  let body = document.querySelector("body");
+  let darkmode = document.getElementbyId("darkmode");
+    darkmode.onclick = function(){
+      body.style.backgroundColor = "black";
+      body.style.color = "white";
+    };
   };
+  // let h1 = document.querySelector("h1");
+  // h1.onmouseover = function() {
+  //   window.alert("I am a heading element.");
+  // };
 
-  let p = document.querySelector("p");
-  p.onmouseover = function() {
-    document.querySelector("p>em").innerText = "Don't be surprised";
-  };
+  // let p = document.querySelector("p");
+  // p.onmouseover = function() {
+  //   document.querySelector("p>em").innerText = "Don't be surprised";
+  // };
 
-  let img = document.querySelector("img");
-  img.onmouseover = function() {
-    img.style.height = "700px";
-  };
-};
+  // let img = document.querySelector("img");
+  // img.onmouseover = function() {
+  //   img.style.height = "700px";
+  // };
